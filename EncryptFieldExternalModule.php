98c93581462689be8e7021d128d1ec1cd5015c80
Jacques Philip <jphilip@alaska.edu>
2020-10-27 22:41:31 -0800

    Changed to file upload private key + decryption in entry form but not encrypt on post

--- EncryptFieldExternalModule.php
+++ EncryptFieldExternalModule.php
@@ -1,111 +1,160 @@
 <?php
 /**
- * REDCap External Module: EncryptField
- * Action tag to encrypt text fields is @ENCRYPT_FIELD.
- * The form holding the tagged field also has to be neabled in the module's project options
+* REDCap External Module: EncryptField
+* Action tag to encrypt text fields is @ENCRYPT_FIELD.
+* The form holding the tagged field also has to be neabled in the module's project options
  * An RSA public key also has to be uploaded in the module's project options
- * Decryption is not done by REDCap at the moment, but R functions are provided to decrypt data
- * @author Jacques Philip, Center for Alaska Native Health Research, University of Alaska Fairbanks
- */
+* Decryption is not done by REDCap at the moment, but R functions are provided to decrypt data
+* @author Jacques Philip, Center for Alaska Native Health Research, University of Alaska Fairbanks
+*/
 
 namespace CANHR\EncryptFieldExternalModule;
 
 use ExternalModules\AbstractExternalModule;
 use ExternalModules\ExternalModules;
 use REDCap;
+use Records;
 
-class EncryptFieldExternalModule extends AbstractExternalModule {
+class EncryptFieldExternalModule extends AbstractExternalModule
+{
+    private $pub_key = '';
 
-  private $pub_key = "";
-
-  public function __construct() {
-    parent::__construct();
-  }
-
-  private function EncryptField($data) {
-    if ($this->pub_key === "") {
-      $this->pub_key = $this->getProjectSetting('public-key');
+    public function __construct()
+    {
+        parent::__construct();
     }
 
-    $sealed = $e = NULL;
-    $iv = openssl_random_pseudo_bytes(32);
-    openssl_seal($data, $sealed, $e, array($this->pub_key), "AES-256-CBC", $iv);
+    private function EncryptField($data)
+    {
+        if ($this->pub_key === '') {
+            $this->pub_key = $this->getProjectSetting('public-key');
+        }
 
-    $payload = base64_encode($sealed);
-    $token = base64_encode($e[0]);
-    $iv = base64_encode($iv);
+        $sealed = $e = null;
+        $iv = openssl_random_pseudo_bytes(32);
+        openssl_seal($data, $sealed, $e, array( $this->pub_key ), 'AES-256-CBC', $iv);
 
-    $j = new \stdClass();
-    $j->payload = $payload;
-    $j->token = $token;
-    $j->iv = $iv;
-    $json = json_encode($j, JSON_UNESCAPED_SLASHES);
-    return($json);
-  }
+        $payload = base64_encode($sealed);
+        $token = base64_encode($e[0]);
+        $iv = base64_encode($iv);
 
-  private function DecryptField($data, $priv_key) {
-    $sealed = json_decode($data);
-
-    $token = base64_decode($sealed->token);
-    $payload = base64_decode($sealed->payload);
-    $iv = base64_decode($sealed->iv);
-    $unsealed = NULL;
-    $pkeyid = openssl_get_privatekey($priv_key);
-    $result = openssl_open($payload, $unsealed, $token, $pkeyid, "AES-256-CBC", $iv);
-    // free the private key from memory (although it stays in Apcu cache until it times out.)
-    openssl_free_key($pkeyid);
-    return($result ? $unsealed : FALSE);
-  }
-
-  function TestPrivateKey($priv_key) {
-    $plain = "Test data to encrypt";
-    $enc = $this->EncryptField($plain);
-    return $this->DecryptField($enc, $priv_key) === $plain;
-  }
-
-  function redcap_every_page_before_render ($project_id){
-    if (PAGE !== "surveys/index.php") {
-      return;
+        $j = new \stdClass();
+        $j->payload = $payload;
+        $j->token = $token;
+        $j->iv = $iv;
+        $json = json_encode($j, JSON_UNESCAPED_SLASHES);
+        return($json);
     }
 
-    if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_GET['s'])) {
-      $hash = $_GET['s'];
-      $sql = "select s.*, h.* from redcap_surveys s, redcap_surveys_participants h, redcap_metadata m
+    private function DecryptField($data, $priv_key)
+    {
+        $sealed = json_decode($data);
+
+        $token = base64_decode($sealed->token);
+        $payload = base64_decode($sealed->payload);
+        $iv = base64_decode($sealed->iv);
+        $unsealed = null;
+        $pkeyid = openssl_get_privatekey($priv_key);
+        $result = openssl_open($payload, $unsealed, $token, $pkeyid, 'AES-256-CBC', $iv);
+        // free the private key from memory ( although it stays in Apcu cache until it times out. )
+        openssl_free_key($pkeyid);
+        return($result ? $unsealed : false);
+    }
+
+    public function TestPrivateKey($priv_key)
+    {
+        $plain = 'Test data to encrypt';
+        $enc = $this->EncryptField($plain);
+        return $this->DecryptField($enc, $priv_key) === $plain;
+    }
+
+    public function redcap_data_entry_form($project_id, $record, $instrument, $event_id, $group_id, $repeat_instance)
+    {
+        $record_id = $record;
+
+        if (!$this->CheckEncForm($instrument)) {
+            return;
+        }
+
+        $fields = \REDCap::getDataDictionary('array', false, true, $instrument);
+
+        $enc_fields = array();
+        foreach ($fields as $this_field) {
+            if (in_array($this_field['field_type'], array( 'text', 'notes' )) && strpos($this_field['field_annotation'], '@ENCRYPT_FIELD') !== false) {
+                $enc_fields[] = $this_field;
+            }
+        }
+        if (count($enc_fields) > 0) {
+            $priv_key = false;
+            $apcu_key = session_id() . "_" . USERID;
+            if (apcu_exists($apcu_key) && $this->TestPrivateKey(apcu_fetch($apcu_key))) {
+                $priv_key = apcu_fetch($apcu_key);
+                # Extend ttl in apcu
+                apcu_store($apcu_key, $priv_key, intval($this->getProjectSetting('private-key-ttl')) * 60);
+                $enc_data = Records::getData($project_id, "array", $record, array_column($enc_fields, "field_name"), $event_id, $group_id);
+                $enc_data = $enc_data[$record][$event_id];
+            }
+            
+            $js = "<script type='text/javascript'>\n";
+            foreach ($enc_fields as $enc_field) {
+                if ($priv_key) {
+                    $dec_str = json_encode($this->DecryptField($enc_data[$enc_field["field_name"]], $priv_key));
+                } else {
+                    $dec_str = "'Please load private key to decrypt data'";
+                    $js .= "\$('[name=${enc_field["field_name"]}]').prop('readonly', true);\n";
+                }
+                $js .= "\$('[name=${enc_field["field_name"]}]').val($dec_str);\n";
+            }
+            $js .= "</script>";
+            echo($js);
+        }
+    }
+
+    private function CheckEncForm($form_name)
+    {
+        //Check if the current form is set for encryption
+        $forms = $this->getProjectSetting('project-form-list');
+        if (!is_array($forms) || count($forms) == 0) {
+            return false;
+        }
+
+        $enc_form = false;
+        foreach ($forms as $form) {
+            if ($form === $form_name) {
+                $enc_form = true;
+                break;
+            }
+        }
+        return $enc_form;
+    }
+
+    public function redcap_every_page_before_render($project_id)
+    {
+        if (strtolower(PAGE) === 'surveys/index.php' && strtoupper($_SERVER['REQUEST_METHOD']) == 'POST' && isset($_GET['s'])) {
+            $hash = $_GET['s'];
+            $sql = "select s.*, h.* from redcap_surveys s, redcap_surveys_participants h, redcap_metadata m
                 where h.hash = '".db_escape($hash)."' and s.survey_id = h.survey_id and m.project_id = s.project_id 
                 and m.form_name = s.form_name and h.event_id is not null limit 1";
-      $q = db_query($sql);
-      $res = db_fetch_assoc($q);
-      if (!$res)
-        return;
-      $survey_id = $res["survey_id"];
-      $form_name = $res["form_name"];
-      $event_id = $res["event_id"];
+            $q = db_query($sql);
+            $res = db_fetch_assoc($q);
+            if (!$res) {
+                return;
+            }
+            $survey_id = $res['survey_id'];
+            $form_name = $res['form_name'];
+            $event_id = $res['event_id'];
 
-      $forms = $this->getProjectSetting('project-form-list');
-      if (!is_array($forms) || count($forms) == 0) {
-        return;
-      }
+            if (!$this->CheckEncForm($form_name)) {
+                return;
+            }
 
-      $enc_form = false;
-      foreach($forms as $form) {
-        if ($form === $form_name) {
-          $enc_form = true;
-          break;
+            $fields = \REDCap::getDataDictionary('array', false, true, $form_name);
+
+            foreach ($fields as $this_field) {
+                if (in_array($this_field['field_type'], array( 'text', 'notes' )) && strpos($this_field['field_annotation'], '@ENCRYPT_FIELD') !== false) {
+                    $_POST[$this_field['field_name']] =  $this->EncryptField($_POST[$this_field['field_name']]);
+                }
+            }
         }
-      }
-
-      if (!$enc_form) {
-        return;
-      }
-
-      $fields = REDCap::getDataDictionary('array', false, true, $form_name);
-
-      foreach ($fields as $this_field) {
-        if(in_array($this_field["field_type"], array("text", "notes")) && strpos($this_field['field_annotation'], "@ENCRYPT_FIELD") !== false) {
-          $_POST[$this_field["field_name"]] =  $this->EncryptField($_POST[$this_field["field_name"]]);
-        }
-      }      
     }
-  }
-
 }
--- key.php
+++ key.php
@@ -9,26 +9,33 @@ $apcu_key = session_id() . "_" . USERID;
 $encryptField = new \CANHR\EncryptFieldExternalModule\EncryptFieldExternalModule();
 $ttl = intval($encryptField->getProjectSetting('private-key-ttl')) * 60;
 
-function PrintKeyForm() {
-    print('<form method="post" class="darkgreen" style="max-width:650px;padding:20px;">');
-    print('<div><label for="key">Paste your decryption key below:</label></div>');
-    print('<div><textarea name="key" id="key" cols="60" rows="30"></textarea></div>');
+function PrintKeyForm()
+{
+    print('<form method="post" enctype="multipart/form-data" class="darkgreen" style="max-width:650px;padding:20px;">');
+    print('<div><label for="key">Select a decryption key file to upload:</label></div>');
+    print('<div><input type="file" name="key" id="key"  required></div>');
     print('<div style="padding-top:10px;"><button type="submit" class="btn btn-xs btn-rcgreen fs14">Continue</button></div>');
     print('</form>');
 }
 ?>
 
 <h2>Decryption Key Import</h2>
-<?php 
-if ($_SERVER['REQUEST_METHOD']=='POST') {
+<?php
+if (strtoupper($_SERVER['REQUEST_METHOD'])=='POST') {
     $errors = array();
-    if (!isset($_POST['key']) || $_POST['key']==='') {
-        $errors[] = "The key cannot be empty";
-    }
-    else
-    {
-        if (!$encryptField->TestPrivateKey($_POST['key'])) {
-            $errors[] = "Invalid decription key.";
+    if (!isset($_FILES['key'])) {
+        $errors[] = "Please select a file.";
+    } else {
+        try {
+            $key= trim(file_get_contents($_FILES['key']['tmp_name']));
+        } catch (\Throwable $th) {
+            //throw $th;
+        } finally {
+            unlink($_FILES["key"]["tmp_name"]);
+        }
+        
+        if (!$encryptField->TestPrivateKey($key)) {
+            $errors[] = "Invalid decryption key.";
         }
     }
 
@@ -38,42 +45,33 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
         if (isset($_SESSION["crypt_key_import_success"])) {
             unset($_SESSION["crypt_key_import_success"]);
         }
-    }        
-    else {
+    } else {
         if (isset($_SESSION["crypt_key_import_error"])) {
             unset($_SESSION["crypt_key_import_error"]);
         }
         $_SESSION["crypt_key_import_success"] = 1;
-        apcu_store($apcu_key, trim($_POST['key']), $ttl);
+        apcu_store($apcu_key, $key, $ttl);
     }
-    header("Location: " . $encryptField->getUrl("key.php"));        
-}
-else
-{
-    if (isset($_SESSION["crypt_key_import_error"])){
+    header("Location: " . $encryptField->getUrl("key.php"));
+} else {
+    if (isset($_SESSION["crypt_key_import_error"])) {
         $errors = $_SESSION["crypt_key_import_error"];
         print('<div class="red" style="margin:20px 0;">');
-            foreach($errors as $error) {
-                print("<p>$error</p>");
-            }
+        foreach ($errors as $error) {
+            print("<p>$error</p>");
+        }
         print('</div>');
         unset($_SESSION["crypt_key_import_error"]);
         PrintKeyForm();
-    }
-    elseif (isset($_SESSION["crypt_key_import_success"])) {
+    } elseif (isset($_SESSION["crypt_key_import_success"])) {
         $ttl_min = round($ttl/60);
-        print('<div class="green" style="margin:20px 0;">The decription key was imported. It will be unloaded after ' . $ttl_min .' minutes of incactivity.</div><br/>');
+        print('<div class="green" style="margin:20px 0;">The decryption key was imported. It will be unloaded after ' . $ttl_min .' minutes of incactivity.</div><br/>');
         unset($_SESSION["crypt_key_import_success"]);
-    }
-    else
-    {        
+    } else {
         if (apcu_exists($apcu_key)) {
-            print('<p class="green" style="margin:20px 0;">A decription key is currently loaded.</p>');
-        }
-        else {
+            print('<p class="green" style="margin:20px 0;">A decryption key is currently loaded.</p>');
+        } else {
             PrintKeyForm();
         }
     }
 }
-
-    
