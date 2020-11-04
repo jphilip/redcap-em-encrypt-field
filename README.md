81a5900ced4e47a787a56b04c0917dc958f8b355
Jacques Philip <jphilip@alaska.edu>
2020-11-03 15:15:27 -0900

    Changes to README and examples

--- README.md
+++ README.md
@@ -9,12 +9,14 @@ Make sure you do not use the demo keys provided in this repository, you need to 
 5. Paste the public key in the module's project options.
 6. Assign the @ENCRYPT\_FIELD smart tag to the fields you want to encrypt (Only text and notes are currently supported).
 7. Some R functions are provided in the package to decrypt individual field values or a cvs file exported by REDCap, using your private key. Implementations in other languages are easy to implement using the examples from the repository: [https://github.com/CodeReaper/unsealed-secrets](https://github.com/CodeReaper/unsealed-secrets)
-8. A basic REDCap project is provided in the package to test the functionality.
+8. A basic REDCap project in XML format is provided in the Github package to demonstrate the functionality. Note that the EM project settings are not part of the XML so you will have to set them: To get started, open project setttings and only enable encryption in the consent instrument and load the public key located in the example folder. The demo data has already been extracted to the EncryptDemo.csv file and decrypted to the decrypted_EncryptDemo.csv file.  
+To do your own decryption, open the decrypt.R file in R or RStudio and execute line by line to follow the flow.  
+You can add records by filling new surveys, then extract the data to a new csv file and decrypt similarly in R.
 
 ## How it works
 
 1. Encryption is performed in PHP similarly to: [https://github.com/CodeReaper/unsealed-secrets](https://github.com/CodeReaper/unsealed-secrets), however it uses a stronger cypher, AES-256 in CBC mode instead of RC4.
-2. The encrypted text is a json string however, when you open the form in REDCap, encrypted fields will be displayed as "Encrypted field (cannot be modified)" and they will be readonly, but the other fields can be modified and saved. The logs though will display the encrypted json string.
+2. The encrypted text is a json string, however when you open the form in REDCap, encrypted fields will be displayed as "Encrypted field (cannot be modified)" and they will be readonly, but the other fields can be modified and saved. The logs though will display the encrypted json string.
 
 ## Limitations
 
--- example/R-decrypt/decrypt.R
+++ example/R-decrypt/decrypt.R
@@ -2,27 +2,33 @@ library(openssl)
 library(rjson)
 
 
-decrypt.field <- function(input) {
+# Individual string decryption
+
+decrypt.field <- function(input, keyfile) {
   tryCatch({
     result <- input
     input.l <- fromJSON(input)
     if (!identical(c("payload", "token", "iv"), names(input.l)))
-      return(input)  
+      stop("Format error")
+      #return(input)  
     input.b <- lapply(input.l, base64_decode)
-    key <- read_key("key.pem")
+    key <- read_key(keyfile)
     plain.b <- decrypt_envelope(input.b$payload, input.b$iv, input.b$token, key)
     result <- rawToChar(plain.b)
   },
-  error=function(e) {print(e)})
+  error=function(e) {cat(sprintf("Invalid encrypted string; skipping '%s'...\n", substr(input, 1, 60)))})
   return(result)
 }
 
-decrypt.csv <- function (f, new.file=paste0("decrypted_", f)) {
+# csv file decryption function
+# Returns a list with the decrypted data frame and the number of fields decrypted
+
+decrypt.csv <- function (f, keyfile, new.file=paste0("decrypted_", f)) {
   report <- read.csv(f, header=T, as.is=T)
   nb.dec <- 0
   for (r in seq(nrow(report))) {
     for (c in seq(ncol(report))) {
-      dec <- decrypt.field(report[r,c])
+      dec <- decrypt.field(report[r,c], keyfile)
       if (!identical(dec, report[r,c])) {
         report[r,c] <- dec
         nb.dec <- nb.dec + 1
@@ -35,11 +41,13 @@ decrypt.csv <- function (f, new.file=paste0("decrypted_", f)) {
 }
 
 
-#### Test deryption with known text encrypted in REDCap
+#### Test decryption with known text encrypted in REDCap
 
 test.enc <- '{"payload":"wRczHB+hc4Dv/cnDMzKBeg==","token":"ndlvI6TVDrkt5WPgnvlY5UWGO9lZL8xBYeEtl0zQ8Q6Uvi+3QRyfc4GH5DqLr0YfpWXzW1C013OlOMHtpcSWx48QVEA5Nx37gIOjIgjTyJZlURWIu6i2bV0u12BhzggwB0e0+vrkfH0+qeXz/Is1R6suFUITzbo6Ax4UYILeCe6No+Isygh8rMvRtxBarbQaejxfokoiv+cS/AmxQJibAC4pYiCd3kp4ZRynLiPEAYBuBhgpJL4TlOUVuUu810iYU570GzA83W7V+Z3TXKUkpfVo7mWnKAQhRT1uUScghAC2807x8xWLWu1w+c4iyLUBQJA/Mo89q30KJ1EqDYQEcRM/yYZmxuRf6SNbPSFIJZU9IGRCFtAFfqsXZmHTko+XFSzCYtfiMhPxLM9CTwgCOeIwrFIHLRmRVwSYq0SJAmC7fog17MMkfe7y5Kxt3QnuLp9WO8JaDqv8sVokvRu7R76wUb7qsooFdyWor9fhqFnSsCEw5l73YbWWK0y01AZ0","iv":"Kxr4ktM8dGbc1EDuDaX+jQ=="}'
 
-test.plain <- decrypt.field(test.enc)
+test.plain <- decrypt.field(test.enc, "key.pem")
 stopifnot(identical(test.plain, "ccccccccccccc"))
 
-dec.list <- decrypt.csv("EncryptDemo.csv")
+# Decrypt the whole demo csv file
+
+dec.list <- decrypt.csv("EncryptDemo.csv", "key.pem")
